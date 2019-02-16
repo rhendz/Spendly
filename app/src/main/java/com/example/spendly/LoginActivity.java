42331d52d58bd11d059f9b433db85a5300a2e3ad
Sai Thatigotla <sai.thatigotla@gmail.com>
2019-02-16 15:51:28 -0500

    updated login view

--- app/src/main/java/com/example/spendly/LoginActivity.java
+++ app/src/main/java/com/example/spendly/LoginActivity.java
@@ -67,7 +67,7 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         // Set up the login form.
-        mEmailView = findViewById(R.id.login);
+        mEmailView = findViewById(R.id.email);
         populateAutoComplete();
 
         mPasswordView = (EditText) findViewById(R.id.password);
@@ -91,7 +91,7 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
         });
 
       //  mLoginFormView = findViewById(R.id.login_form);
-        mProgressView = findViewById(R.id.login_progress);
+        //mProgressView = findViewById(R.id.login_progress);
     }
 
     private void populateAutoComplete() {
